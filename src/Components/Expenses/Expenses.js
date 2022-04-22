260470a548827271524354ea97678a3c86219170
Kles <klesta.luli@gmail.com>
2022-04-22 15:20:37 +0300

    organized code

--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -10,9 +10,21 @@ export default function Expenses(props) {
                 amount={props.items[0].amount}
                 date={props.items[0].date}
             />
-            <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date} />
-            <ExpenseItem title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date} />
-            <ExpenseItem title={props.items[3].title} amount={props.items[3].amount} date={props.items[3].date} />
+            <ExpenseItem 
+                title={props.items[1].title} 
+                amount={props.items[1].amount} 
+                date={props.items[1].date} 
+            />
+            <ExpenseItem 
+                title={props.items[2].title} 
+                amount={props.items[2].amount} 
+                date={props.items[2].date} 
+            />
+            <ExpenseItem 
+                title={props.items[3].title} 
+                amount={props.items[3].amount} 
+                date={props.items[3].date} 
+            />
         </Card>
     )
 }
