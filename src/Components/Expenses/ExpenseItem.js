bf37d5615462bc7686109460bacb0515d9439ac1
Kles <klesta.luli@gmail.com>
2022-04-26 12:54:45 +0300

    deleted practice btn

--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -1,23 +1,14 @@
 import "./ExpenseItem.css"
 import ExpenseDate from "./ExpenseDate"
-import { useState } from "react"
 
 export default function ExpenseItem(props) {
-    const [title, setTitle] = useState(props.title)
-    
-    function handleTitle() {
-        setTitle('shampoo')
-        console.log(title)
-    }
-
     return (
         <div className="expense-item">
             <ExpenseDate date={props.date} />
             <div className="expense-item__description">
-                <h2>{title}</h2>
+                <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
-            <button onClick={handleTitle}>Change title</button>
         </div>
     )
 }
